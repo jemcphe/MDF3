c5c3cdcf78efa250514b7c4613037f4a8029d38b
James McPherson <jemcphe@fullsail.edu>
2013-07-27 20:26:41 -0400

    Making commit to ensure that all assets are accounted for before submission.

--- Week3 Widget/TeamWidget/gen/com/jemcphe/teamwidget/R.java
+++ Week3 Widget/TeamWidget/gen/com/jemcphe/teamwidget/R.java
@@ -24,10 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090000;
+        public static final int action_settings=0x7f090007;
+        public static final int button1=0x7f090000;
+        public static final int runs1=0x7f090004;
+        public static final int runs2=0x7f090006;
+        public static final int team1=0x7f090003;
+        public static final int team2=0x7f090005;
+        public static final int teamGrid=0x7f090002;
+        public static final int textView1=0x7f090001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int widget_layout=0x7f030001;
     }
     public static final class menu {
         public static final int main=0x7f080000;
